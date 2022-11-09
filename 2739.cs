383efe8cf43ad61f8eadd5c65bdeccf8a381da56
absd9515 <liououo1@naver.com>
2022-11-09 14:58:11 +0900

    2739번 문제 풀음

---
+++ 2739.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            
+            for (int i = 1; i <= 9; i++)
+            {
+               
+                Console.WriteLine($"{s} * {i} = {int.Parse(s)*i}");
+                
+            }
+        }
+    }
+}
