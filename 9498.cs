3f9caec00dd325fe08e8345c278ea50e4c135865
absd9515 <liououo1@naver.com>
2022-11-09 16:53:48 +0900

    9498문제 풀음

---
+++ 9498.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+
+namespace baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            int A = 100;
+            int B = 89;
+            int C = 79;
+            int D = 70;
+            int E = 60;
+
+
+            if(number > B)
+            {
+                Console.WriteLine("A");
+            }
+            else if(number > C)
+            {
+                Console.WriteLine("B");
+            }
+            else if (number >= D)
+            {
+                Console.WriteLine("C");
+            }
+            else if (number >= E)
+            {
+                Console.WriteLine("D");
+            }
+            else
+            {
+                Console.WriteLine("F");
+            }
+        }
+    }
+}
