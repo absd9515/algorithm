b8779472a28503669b55b2dcff297e1fe2682af4
absd9515 <liououo1@naver.com>
2022-11-09 16:12:30 +0900

    2741번 문제 풀음

---
+++ 2741.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+   
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            StringBuilder result = new StringBuilder();
+
+            for(int i = 1; i<=number; i++)
+            {
+                result.Append($"{i}\n");
+            }
+
+
+            Console.Write(result);
+         
+
+            //for(int i =1; i <= int.Parse(s); i++)
+            //{              
+            //    Console.Write($"{i}\n");        
+            //}
+        }
+    }
+}
