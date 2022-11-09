6f7e29c86594a81cb76257b0c4d18bcbc1c4667b
absd9515 <liououo1@naver.com>
2022-11-09 14:41:19 +0900

    2475번 문제 품

---
+++ 2475.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] s = Console.ReadLine().Split();
+
+            int A = int.Parse(s[0]);
+            int B = int.Parse(s[1]);
+            int C = int.Parse(s[2]);
+            int D = int.Parse(s[3]);
+            int E = int.Parse(s[4]);
+
+            A *= A;
+            B *= B;
+            C *= C;
+            D *= D;
+            E *= E;
+
+            Console.WriteLine((A+B+C+D+E) % 10);
+        }
+    }
+}
