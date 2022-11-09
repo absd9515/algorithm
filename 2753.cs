0f9a8bfd970b2787230e69331effc223702e2cc6
absd9515 <liououo1@naver.com>
2022-11-09 16:37:30 +0900

    2753문제 풀음

---
+++ 2753.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace baekjoon
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if(number % 4 == 0)
+            {
+                if(number % 100 != 0 || number % 400 == 0)
+                {
+                    Console.WriteLine("1");
+                }     
+                else
+                {
+                    Console.WriteLine("0");
+                }
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+        }
+    }
+}
